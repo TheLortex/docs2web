b5c5bbcbe9923c18c08208f3c46314f335d9cd1a
Lucas Pluvinage <lucas@tarides.com>
2021-05-06 17:11:21 +0200

    update to dream~alpha2, add cache control for static files

--- bin/docs2web.ml
+++ bin/docs2web.ml
@@ -26,59 +26,60 @@ let packages_scope ~state kind =
   [
     Dream.get "/" (fun _ -> respond (Packages.v ~state));
     Dream.get "/index.html" (fun _ -> respond (Packages.v ~state));
-    Dream.get "/*" (fun request ->
+    Dream.get "/**" (fun request ->
         let kind = get_kind request in
-        match Fpath.of_string (Dream.path request) with
-        | Ok path -> (
-            Dream.log "Path is: %s => %s" (Fpath.to_string path)
-              (Fpath.segs path |> String.concat "--");
-            match Fpath.segs path with
-            | [ ""; package ] | [ ""; package; "" ] -> (
-                let name =
-                  try OpamPackage.Name.of_string package
-                  with Failure _ ->
-                    OpamPackage.Name.of_string "non-existent-package"
+        Dream.log "%s" (Dream.path request |> String.concat "--");
+        match Dream.path request with
+        | [ package ] | [ package; "" ] -> (
+            let name =
+              try OpamPackage.Name.of_string package
+              with Failure _ ->
+                OpamPackage.Name.of_string "non-existent-package"
+            in
+            let* version = find_default_version state name in
+            match version with
+            | Some version ->
+                let target =
+                  Dream.prefix request ^ "/" ^ package ^ "/"
+                  ^ OpamPackage.Version.to_string version
+                  ^ "/"
                 in
-                let* version = find_default_version state name in
-                match version with
-                | Some version ->
-                    let target =
-                      Dream.prefix request ^ "/" ^ package ^ "/"
-                      ^ OpamPackage.Version.to_string version
-                      ^ "/"
-                    in
-                    Lwt.return (redirect ~target)
-                | None -> not_found () )
-            | [ ""; package; version ] ->
-                Lwt.return
-                  (redirect
-                     ~target:
-                       ( Dream.prefix request ^ "/" ^ package ^ "/" ^ version
-                       ^ "/" ))
-            | "" :: package :: version :: path -> (
-                let name =
-                  try OpamPackage.Name.of_string package
-                  with Failure _ ->
-                    OpamPackage.Name.of_string "non-existent-package"
-                in
-                let version =
-                  try OpamPackage.Version.of_string version
-                  with Failure _ -> OpamPackage.Version.of_string "0"
-                in
-                let path = String.concat "/" path in
-                try
-                  respond (Package.v ~state ~kind ~name ~version ~path ())
-                with
-                | Not_found -> not_found ()
-                | Failure _ -> not_found () )
-            | _ -> Dream.respond ~status:`Internal_Server_Error "" )
-        | Error _ -> not_found ());
+                Lwt.return (redirect ~target)
+            | None -> not_found () )
+        | [ package; version ] ->
+            Lwt.return
+              (redirect
+                 ~target:
+                   (Dream.prefix request ^ "/" ^ package ^ "/" ^ version ^ "/"))
+        | package :: version :: path -> (
+            let name =
+              try OpamPackage.Name.of_string package
+              with Failure _ ->
+                OpamPackage.Name.of_string "non-existent-package"
+            in
+            let version =
+              try OpamPackage.Version.of_string version
+              with Failure _ -> OpamPackage.Version.of_string "0"
+            in
+            let path = String.concat "/" path in
+            try respond (Package.v ~state ~kind ~name ~version ~path ()) with
+            | Not_found -> not_found ()
+            | Failure _ -> not_found () )
+        | _ -> Dream.respond ~status:`Internal_Server_Error "");
   ]
 
+
+let cache_header handler request =
+  let open Lwt.Syntax in
+  let+ response = handler request in 
+  Dream.with_header "Cache-Control" "public, max-age=3600, immutable" response 
+  
 let job =
   let* state = Docs2web.State.v () in
-  Dream.log "Ready to serve at http://localhost:%d%s" 8082 Docs2web.Config.prefix;
-  Dream.serve ~port:8082 ~prefix:Docs2web.Config.prefix @@ Dream.logger
+  Dream.log "Ready to serve at http://localhost:%d%s" 8082
+    Docs2web.Config.prefix;
+  Dream.serve ~port:8082 ~prefix:Docs2web.Config.prefix
+  @@ Dream.logger
   @@ Dream.router
        [
          Dream.get "/" (fun _ -> respond (Docs2web_pages.Index.v ~state));
@@ -88,7 +89,9 @@ let job =
              Dream.get "/" (fun _ -> Dream.respond "universes");
              Dream.scope "/:hash" [] (packages_scope ~state Universes);
            ];
-         Dream.get "/static/*" @@ Dream.static "static";
+         Dream.scope "/static" [cache_header] [
+           Dream.get "/**" @@ Dream.static "static";
+          ]
        ]
   @@ not_found
 
